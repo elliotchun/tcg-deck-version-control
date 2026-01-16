d1e3b2944fcd30f1dd8502e790221938ad7b640a
elliotchun <eschun@uci.edu>
2026-01-16 14:23:03 -0800

    Implement deck

--- src/deck.ts
+++ src/deck.ts
@@ -1,6 +1,28 @@
+type TDeck = Record<string, number>
+type TCardName = string
+
 export default class Deck {
-    addCard(name: string, quantity = 1) {}
-    removeCard(name: string, quantity = 1) {}
-    numberOfCards(): number { return -1 }
-    numberOf(name: string): number { return -1 }
+    #deck: TDeck;
+
+    constructor() {
+        this.#deck = {};
+    }
+
+    addCard(name: TCardName, quantity = 1) {
+        this.#deck[name] = this.numberOf(name) + quantity;
+    }
+    removeCard(name: TCardName, quantity = 1) {
+        if (this.#deck[name]) {
+            this.#deck[name] -= quantity;
+            if (this.#deck[name] <= 0) {
+                delete this.#deck[name];
+            }
+        }
+    }
+    numberOfCards(): number {
+        return Object.values(this.#deck).reduce((total, quantity) => total + quantity, 0);
+    }
+    numberOf(name: TCardName): number {
+        return this.#deck[name] || 0
+    }
 }
